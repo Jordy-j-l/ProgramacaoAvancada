5deac38c04541d25e7db5036c5472163ef4d1d54
Jordy <122643228+Jordy-j-l@users.noreply.github.com>
2025-05-13 12:58:43 +0100

    Refazendo tudo
    I need cry

--- projetoAndroidStudio/ContinentesApp/app/src/main/java/com/example/continentesapp/MainActivity.kt
+++ projetoAndroidStudio/ContinentesApp/app/src/main/java/com/example/continentesapp/MainActivity.kt
@@ -1,45 +1,33 @@
 package com.example.continentesapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.continentesapp.ui.theme.ContinentesAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val btnAfrica = findViewById<Button>(R.id.btnAfrica)
-        val btnEuropa = findViewById<Button>(R.id.btnEuropa)
-        val btnAmerica = findViewById<Button>(R.id.btnAmerica)
-        val btnAsia = findViewById<Button>(R.id.btnAsia)
-
-        val intent = Intent(this, ContinenteActivity::class.java)
-
-        btnAfrica.setOnClickListener {
-            val intent = Intent(this, ContinenteActivity::class.java)
-            intent.putExtra("continente", "africa")
-            startActivity(intent)
+        setContent {
+            ContinentesAppTheme {
+                ContinentesApp()
+            }
         }
+    }
+}
 
-        btnEuropa.setOnClickListener {
-            val intent = Intent(this, ContinenteActivity::class.java)
-            intent.putExtra("continente", "europa")
-            startActivity(intent)
-        }
+@Composable
+fun ContinentesApp() {
+    Text("Olá Continentes!", style = MaterialTheme.typography.headlineMedium)
+}
 
-        btnAmerica.setOnClickListener {
-            val intent = Intent(this, ContinenteActivity::class.java)
-            intent.putExtra("continente", "america")
-            startActivity(intent)
-        }
-
-        btnAsia.setOnClickListener {
-            val intent = Intent(this, ContinenteActivity::class.java)
-            intent.putExtra("continente", "asia")
-            startActivity(intent)
-        }
-
+@Preview(showBackground = true)
+@Composable
+fun ContinentesAppPreview() {
+    ContinentesAppTheme {
+        ContinentesApp()
     }
 }
